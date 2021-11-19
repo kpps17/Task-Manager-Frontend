771bd0b5c60b34bb561c494b92cce274304aeb43
kpps17 <sinhaarchit17@gmail.com>
2021-11-20 01:16:32 +0530

    signup page material ui added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import Login from "./Components/Login/Login";
 import Signup from "./Components/Signup/Signup";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 
 function App() {
 	return (
-		<div className="App">
-			<Signup />
-		</div>
+		<Router>
+			<div className='App'>
+				<Switch>
+					<Route path='/signup'component={Signup} exact></Route>
+					<Route path='/login'component={Login} exact></Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,51 +1,96 @@
 import { useState } from "react";
 import axios from 'axios';
+import { Avatar, Container, Box, Typography, Grid, TextField, Button, Link } from '@mui/material';
+import { LockClockOutlined } from "@mui/icons-material";
+import { useHistory } from "react-router";
 
 const Signup = () => {
-    
+
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
     const [password, setPassword] = useState("");
-    const [message, setMessage] = useState("");
 
-    const handleSignUp = () =>{
+    const history = useHistory();
+
+    const handleSignUp = () => {
         try {
-            const userData = {name: name, email: email, password: password};
+            const userData = { name: name, email: email, password: password };
             axios.post('/users', userData).then((res) => {
                 console.log(res);
                 if(res.data.error) {
-                    setMessage('invalid');
+                    window.alert('Email Id already registered');
                 } else {
-                    setMessage('valid')
+                    history.push('/login');
                 }
-            }).catch(err => setMessage(err.message)); 
-            
+            }).catch((err) => {
+                
+            })
         } catch (err) {
             console.log(err);
         }
     }
-    
-    return (  
-        <div>
-            <h1>Signup Page</h1>
-            <div>
-                <div>
-                    Name
-                    <input value={name} onChange={(e) => setName(e.target.value)}></input>
-                </div>
-                <div>
-                    Email
-                    <input value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                </div>
-                <div>
-                    Password
-                    <input value={password} onChange={(e) => setPassword(e.target.value)}></input>
-                </div>
-            </div>
-            <button onClick={handleSignUp}>SignUp</button>
-            <h2>{message}</h2>
-        </div>
+
+    return (
+        <Container component='div' maxWidth='xs'>
+            <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <Avatar sx={{ m: 2, bgcolor: 'secondary.main' }}>
+                    <LockClockOutlined />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <Box component="form" sx={{ mt: 3 }}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="given-name"
+                                name="firstName"
+                                required
+                                fullWidth
+                                id="firstName"
+                                label="First Name"
+                                autoFocus
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="new-password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button variant="contained" sx={{ mt: 3, mb: 2 }} fullWidth onClick={handleSignUp}>
+                        Sign up
+                    </Button>
+                    <Grid container sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', mt: 3}}>
+                        <Grid item>
+                            <Link href="/login">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+            </Box>
+        </Container>
     );
 }
- 
+
 export default Signup;
